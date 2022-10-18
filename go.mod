a03aa455c7a97fea148d09a16fea25bb102deadb
yu-ko-ba <yu-ko-ba@outlook.com>
2022-10-18 15:04:04 +0900

    完成

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/tacg0909/meshitero-get-user-post-outlines
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go v1.42.47
+	github.com/guregu/dynamo v1.17.0
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+func main() {
+    lambda.Start(meshiteroGetUserPostOutlines)
+}
+
+type user struct {
+    Id string `json:"userId"`
+}
+
+type userPostOutline struct {
+    PostedTime string `json:"postedTime"`
+    SmallImageUrl string `json:"smallImageUrl"`
+    Id string `json:"id"`
+}
+
+func meshiteroGetUserPostOutlines(user user) ([]userPostOutline, error) {
+    db := dynamo.New(
+        session.New(), 
+        &aws.Config{
+            Region: aws.String(os.Getenv("DYNAMO_REGION")),
+        },
+    )
+    table := db.Table(os.Getenv("TABLE_NAME"))
+
+    var outlines []userPostOutline
+    err := table.Get(os.Getenv("PARTITION_KEY_NAME"), user.Id).All(&outlines)
+    return outlines, err
+}
